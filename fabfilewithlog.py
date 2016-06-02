65187164d400a90e6cb0e4827b752515443baf9e
Aldo Sotolongo <aldenso@gmail.com>
2016-06-02 14:51:07 -0430

    fab file for python fabric, including ouput log

---
+++ fabfilewithlog.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import datetime
+from fabric.api import local, run, env
+
+env.hosts = ['192.168.125.100', '192.168.125.20']
+datelog = datetime.datetime.now().strftime("%d%m%y-%H%M%S")
+
+def log(command, msg):
+    logfile=open("output_"+datelog+".txt","a+")
+    logfile.write("server:" + env.host + ":command:" + command + "\n")
+    logfile.write(msg + "\n")
+    logfile.close()
+
+def uptime():
+    up = run('uptime')
+    log('uptime', up)
+
+def spacefs():
+    space = run('df -h')
+    log('df -h', space)
+
+def inodefs():
+    inode = run('df -i')
+    log('df -i', inode)
+
+def lsdir():
+    ls = run('ls /root/re*')
+    log('ls /root/re*', ls)
+
+def checkfs():
+    uptime()
+    spacefs()
+    inodefs()
+    lsdir()
