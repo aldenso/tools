9460f39510cb2497683531f35229ac4ffe8e9177
Aldo Sotolongo <aldenso@gmail.com>
2016-08-23 21:48:27 -0400

    added inode usage check to filesystemalert_v2.py

--- filesystemalert_v2.py
+++ filesystemalert_v2.py
@@ -3,7 +3,7 @@
 # @Date:   2016-05-25T14:22:59-04:00
 # @Email:  aldenso@gmail.com
 # @Last modified by:   Aldo Sotolongo
-# @Last modified time: 2016-08-23T20:34:17-04:00
+# @Last modified time: 2016-08-23T21:47:23-04:00
 # File: filesystemalert_v2.py
 # Description: This script is useful to set filesystem alerts based on
 # defined thresholds, also you can exclude fs from the alerts
@@ -21,14 +21,21 @@ temporalfile = "/tmp/fsalert.txt"
 excludefs = None
 # excludefs=["/boot","/fsdump"]
 
-# Set thresholds
-yellow = 85
-orange = 90
-red = 95
+# Set thresholds fs
+yellowfs = 85
+orangefs = 90
+redfs = 95
+# Set thresholds inodes
+yellowinode = 85
+orangeinode = 90
+redinode = 95
 # FS in alert, the first element is the header
 fsalert = ["Alert type\tFileSystem\tUsage\n"]
+# FS in alert, the first element is the header
+inodealert = ["Alert type\tFileSystem\tUsage\n"]
 
 filesystems = []
+# if some filesystem is a raid it may be shown more than once in the output.
 cmd = "lsblk"
 p = Popen(cmd, stdout=PIPE, shell=True)
 output, error = p.communicate()
@@ -40,7 +47,8 @@ for i in filesystems:
         filesystems.remove(i)
 
 
-def checkusage(filesystems):
+def checkusagefs(filesystems):
+    print("### FS check ###")
     if excludefs:
         for e in excludefs:
             filesystems.remove(e)
@@ -49,21 +57,49 @@ def checkusage(filesystems):
         p = Popen(cmd, stdout=PIPE, shell=True)
         output, error = p.communicate()
         usage = int(output.split()[4][:-1])
-        if usage >= yellow and usage < orange:
-            fsalert.append("YELLOW ALERT\t{0}\t\t{1}\n".format(fs, usage))
-        elif usage >= orange and usage < red:
-            fsalert.append("ORANGE ALERT\t{0}\t\t{1}\n".format(fs, usage))
-        elif usage >= red:
-            fsalert.append("RED ALERT\t{0}\t\t{1}\n".format(fs, usage))
+        if usage >= yellowfs and usage < orangefs:
+            fsalert.append("yellow FS ALERT\t{0}\t\t{1}\n".format(fs, usage))
+        elif usage >= orangefs and usage < redfs:
+            fsalert.append("orange FS ALERT\t{0}\t\t{1}\n".format(fs, usage))
+        elif usage >= redfs:
+            fsalert.append("red FS ALERT\t{0}\t\t{1}\n".format(fs, usage))
         else:
             print("{}:normal usage".format(fs))
 
 
-def sendmail(fsalert):
-    fsalert.sort()
+def checkusageinode(filesystems):
+    print("### Inode check ###")
+    if excludefs:
+        for e in excludefs:
+            filesystems.remove(e)
+    for fs in filesystems:
+        cmd = "df -i {} | tail -1".format(fs)
+        p = Popen(cmd, stdout=PIPE, shell=True)
+        output, error = p.communicate()
+        usage = int(output.split()[4][:-1])
+        if usage >= yellowinode and usage < orangeinode:
+            inodealert.append("yellow Inode ALERT\t{0}\t\t{1}\n"
+                              .format(fs, usage))
+        elif usage >= orangeinode and usage < redinode:
+            inodealert.append("orange Inode ALERT\t{0}\t\t{1}\n"
+                              .format(fs, usage))
+        elif usage >= redinode:
+            inodealert.append("red Inode ALERT\t{0}\t\t{1}\n"
+                              .format(fs, usage))
+        else:
+            print("{}:normal usage".format(fs))
+
+
+def sendmail(fsalert=None, inodealert=None):
     with open(temporalfile, "wb") as file:
-        for entry in fsalert:
-            file.write(entry)
+        if fsalert is not None:
+            fsalert.sort()
+            for entry in fsalert:
+                file.write(entry)
+        if inodealert is not None:
+            inodealert.sort()
+            for entry in inodealert:
+                file.write(entry)
     fp = open(temporalfile, "rb")
     msg = MIMEText(fp.read())
     msg['From'] = fromaddr
@@ -78,10 +114,23 @@ def sendmail(fsalert):
 
 
 def main():
-    checkusage(filesystems)
-    if len(fsalert) > 1:
-        sendmail(fsalert)
+    checkusagefs(filesystems)
+    checkusageinode(filesystems)
+    if len(fsalert) > 1 and len(inodealert) > 1:
+        sendmail(fsalert, inodealert)
         for entry in fsalert:
+            print(entry)
+        for entry in inodealert:
+            print(entry)
+        syslog.syslog("File System Alert, mail sent to admins")
+    elif len(fsalert) > 1 and len(inodealert) == 1:
+        sendmail(fsalert, None)
+        for entry in fsalert:
+            print(entry)
+        syslog.syslog("File System Alert, mail sent to admins")
+    elif len(fsalert) == 1 and len(inodealert) > 1:
+        sendmail(None, inodealert)
+        for entry in inodealert:
             print(entry)
         syslog.syslog("File System Alert, mail sent to admins")
     else:
